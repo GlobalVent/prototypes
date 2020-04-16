c31fe462216f4ce78907521a1b28a0321c48356e
ralphbellofattoSTX <ralphbel@siliconstx.com>
2020-04-16 06:45:12 -0400

    https://github.com/JamVent/wiki/issues/15 try just adding everything for the prototype directory. we can sort this out later

---
+++ qtStarterApps/qtStarterAppCross/VisualGDB/Debug/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,7 @@
+# Additional clean files
+
+file(REMOVE_RECURSE
+  "CMakeFiles\\qtStarterAppCross_autogen.dir\\AutogenOldSettings.txt"
+  "CMakeFiles\\qtStarterAppCross_autogen.dir\\ParseCache.txt"
+  "qtStarterAppCross_autogen"
+)
