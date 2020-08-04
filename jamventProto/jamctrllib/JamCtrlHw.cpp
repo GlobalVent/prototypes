4ea1e8daf56df977e501f91ddf9ef8e401a5ffe3
carlyjb <carlyjb+github@gmail.com>
2020-08-03 21:35:47 -0400

    very basic completely empty skeleton for JamCtrlHw class, mostly to test interface bw visual studio and github

---
+++ jamventProto/jamctrllib/JamCtrlHw.cpp
@@ -0,0 +1,28 @@
+
+#include "JamCtrlHw.h"
+#include "fmtstr.h"
+
+using namespace std;
+
+
+
+JamCtrlHw::JamCtrlHw()
+{
+
+}
+JamCtrlHw::~JamCtrlHw()
+{
+    
+}
+
+void JamCtrlHw::init() {
+	JamCtrlMgr::init();
+}
+
+/**
+ * @brief 
+ * @param dt delta time since last time we were called.
+ */
+void JamCtrlHw::step(double dt) {
+
+}
